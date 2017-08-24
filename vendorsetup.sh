b941fa6bdaf6ade121dcd6bdb4ffa3ef6f5201b8
GuaiYiHu <guaiyihu@foxmail.com>
2017-08-24 16:51:02 +0800

    virgo: add patch for sensor
    Change-Id: I4f3fc74ed6dac37d3c6087d91765f8d106c946cc

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,10 @@
 add_lunch_combo lineage_virgo-eng
 add_lunch_combo lineage_virgo-userdebug
 
+applyminotepatch() {
+	sh device/xiaomi/virgo/patch/applypatch.sh
+}
+
 export WITH_SU=true
 
 export TMPDIR=/home/guaiyihu/android/laos13/tmp
